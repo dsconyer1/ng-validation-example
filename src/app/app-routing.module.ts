9eaa642925ec1fd9440cbf40622c87c96e65d1a4
Jeff Gonzalez <jeff@Hypertheory>
2019-02-25 09:19:36 -0500

    Added feature and setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: '**',
+    component: FourOhFourComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplatedModule } from './features/templated/templated.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FourOhFourComponent } from './components/four-oh-four/four-oh-four.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
+    TemplatedModule,
     AppRoutingModule
   ],
   providers: [],
